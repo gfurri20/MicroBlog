77144a65712dc1d205ec6141d5ff2e424c9e2e59
gfurri20 <geremia.furri@gmail.com>
2020-02-21 19:03:11 +0100

    Update MicroBlogApplication.java

--- MicroBlog/src/main/java/it/gfurri20/blog/MicroBlogApplication.java
+++ MicroBlog/src/main/java/it/gfurri20/blog/MicroBlogApplication.java
@@ -3,11 +3,14 @@ package it.gfurri20.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MicroBlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroBlogApplication.class, args);
-	}
+@SpringBootApplication
+public class MicroBlogApplication
+{
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run(MicroBlogApplication.class, args);
+    }
 
 }
