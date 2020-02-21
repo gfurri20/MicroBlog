68ce0f768d1d2526f5a2ee83ae114ec87e99d9ce
gfurri20 <geremia.furri@gmail.com>
2020-02-21 18:48:44 +0100

    Added maven project

---
+++ MicroBlog/src/main/java/it/gfurri20/blog/MicroBlogApplication.java
@@ -0,0 +1,13 @@
+package it.gfurri20.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBlogApplication.class, args);
+	}
+
+}
---
+++ MicroBlog/src/test/java/it/gfurri20/blog/MicroBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gfurri20.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
