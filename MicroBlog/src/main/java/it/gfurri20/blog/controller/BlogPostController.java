b80ff3639af4c08d8ea3c48a5aa09ade4cf64549
gfurri20 <geremia.furri@gmail.com>
2020-02-27 13:43:49 +0100

    simple view for all post

--- MicroBlog/src/main/java/it/gfurri20/blog/controller/BlogPostController.java
+++ MicroBlog/src/main/java/it/gfurri20/blog/controller/BlogPostController.java
@@ -3,11 +3,11 @@ package it.gfurri20.blog.controller;
 
 import it.gfurri20.blog.domain.BlogPost;
 import it.gfurri20.blog.service.BlogPostService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,9 +26,10 @@ public class BlogPostController
     BlogPostService blogPostService;
     
     @RequestMapping(value = "all", method = RequestMethod.GET)
-    public ResponseEntity<List<BlogPost>> getAllPosts()
+    public String getAllPosts(Model model)
     {
-        return new ResponseEntity<>(blogPostService.getAllPosts(), HttpStatus.OK);
+        model.addAttribute("posts", blogPostService.getAllPosts());
+        return "view-posts";
     }
     
     @RequestMapping(value = "single/{id}", method = RequestMethod.GET)
